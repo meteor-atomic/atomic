0b67345ff12e7381911828650bc6fe3b8e27d036
Robert Pitt <rpitt@centiq.co.uk>
2015-06-19 17:05:47 +0100

    Removed accounts use call

--- packages/accounts/package.js
+++ packages/accounts/package.js
@@ -12,6 +12,11 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
+
+  // Currently we are only looking at surporting google
+  // in the future we will open this up to twitter
   api.imply("accounts-google");
-  api.addFiles('accounts.js');
+
+  // We use roles to manage permissions
+  api.imply("alanning:roles@1.2.12");
 });
