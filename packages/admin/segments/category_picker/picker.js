42a1f2c12ae6ad49d8a461d8ae04b58b6805d697
Robert Pitt <rpitt@centiq.co.uk>
2015-06-25 03:07:25 +0100

    Fixed category picker and implemented on save

--- packages/admin/segments/category_picker/picker.js
+++ packages/admin/segments/category_picker/picker.js
@@ -1,23 +1,33 @@
 /**
- * Helpers
+ * Store the template instance
  */
-Template.AdminCategoryPickerSegment.created = function(){
-  /**
-   * Subscribe to the tags
-   */
+var View = Template.AdminCategoryPickerSegment;
+
+/**
+ * Created handler
+ * @return {Function}
+ */
+View.created = function() {
+  // Subscribe to all categories
   this.subscribe("categories", Categories.subscription());
 
-  /**
-   * Subscribe to the context
-   */
-  if(this.data.id){
+  // If this category picker has an post id, automatically
+  // pre-populate and update the posts
+  if(this.data.id) {
     this.subscribe("post",
-      Posts.subscription({admin: true, query: {_id: this.data.id} })
+      Posts.subscription({admin: true, query: {_id: this.data.id}})
     );
   }
 }
 
-Template.AdminCategoryPickerSegment.helpers({
+/**
+ * Once rendered bind events and setup UI
+ * @return {Function}
+ */
+View.rendered = function() {
+}
+
+View.helpers({
   /**
    * Return the categories
    * @return {Cursor}
@@ -26,7 +36,22 @@ Template.AdminCategoryPickerSegment.helpers({
     return Categories.all();
   },
 
-  enabled: function() {
-    return Posts.get(Template.instance().data.id).categories.indexOf(this._id) > -1
+  /**
+   * Return true if the category id is within the psots categroies
+   * @return {Boolean}
+   */
+  checked: function() {
+    var post_id = Template.instance().data.id;
+    return Posts.get(post_id).categories.indexOf(this._id) > -1
+  }
+});
+
+View.events({
+  "click input[type=checkbox]": function(e, template) {
+    var checked = e.currentTarget.checked;
+    Posts[checked ? "addCategory" : "removeCategory"](Template.instance().data.id, this._id, function(err) {
+      if(err)
+        return Notify.exception(err);
+    });
   }
 })
