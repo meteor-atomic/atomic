c9bfa021857e1e3b0f370fb78b60c1c7fc8acdf8
Robert Pitt <rpitt@centiq.co.uk>
2015-06-27 22:52:39 +0100

    Added get/getProfile to users model

--- packages/users/common/users.js
+++ packages/users/common/users.js
@@ -6,5 +6,20 @@ _.extend(Users, {
    * Add the collection
    * @type {Meteor}
    */
-  collection:  Meteor.users
+  collection:  Meteor.users,
+
+
+  /**
+   * Fetch a user account
+   */
+  get: function(id, callback) {
+    return Users.collection.findOne({_id: id}, {}, callback);
+  },
+
+  /**
+   * Fetch a user account
+   */
+  getProfile: function(id, callback) {
+    return Users.get(id, callback).profile;
+  }
 });
